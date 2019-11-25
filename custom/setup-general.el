263eb79fb835c8b6bb8e804060770755d6716b49
Neil Chen <jingsi.chen@qq.com>
2019-11-25 22:22:37 +0800

    :-) mac emacs config start now
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ custom/setup-general.el
@@ -0,0 +1,52 @@
+;; disable toolbar
+(tool-bar-mode -1)
+
+;; disable menu bar
+(menu-bar-mode -1)
+
+;; disable scroll bar
+(scroll-bar-mode -1)
+
+;; show column number
+(column-number-mode t)
+
+;; disable startup screen
+(setq inhibit-startup-screen t)
+
+;; change scratch message
+(setq initial-scratch-message ";; Happy Hacking\n\n")
+
+;; disable backup file
+(setq make-backup-files nil)
+
+;; set title
+(setq frame-title-format "P(A|B) = P(B|A)*P(A) / P(B)")
+
+;; disable dialog box
+(setq use-dialog-box nil)
+
+;; addds a new line at the end of the buffer
+;; for fixing: "\ No newline at end of file" issue
+(setq require-final-newline t)
+
+;; use y/n, via symbol indication process
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; highlight current line
+(global-hl-line-mode 1)
+
+
+;; keep a list of recently opened files
+(recentf-mode 1)
+(global-set-key (kbd "<f4>") 'recentf-open-files)
+
+;; check if emacs is running in graphical environment (not terminal)
+(if (display-graphic-p)
+    (progn
+      (set-frame-font "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+      (load-theme 'gandalf t)
+      (setq default-frame-alist
+	    '((width . 106)))))
+
+;;; $
+(provide 'setup-general)
---
+++ init.el
@@ -0,0 +1,32 @@
+;;; disalbe emacs making changes, and keep the settings in a separate file
+(setq custom-file (concat user-emacs-directory "custom-set-variables.el"))
+(load custom-file 'noerror)
+
+;;; init package
+;; use M-x package-list-packages
+;; to browse and install packages from MELPA and elsewhere
+(require 'package)
+(setq package-archives '(("gnu-cn"   . "https://elpa.emacs-china.org/gnu/")
+                         ("melpa-cn" . "https://elpa.emacs-china.org/melpa/")))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;;; load configurations
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/custom"))
+
+(defmacro require-init (pkg)
+  "Load self init PKG."
+  `(load (file-truename (format "~/.emacs.d/custom/%s", pkg))))
+
+(require-init 'setup-general)
+
+
+;;; $
