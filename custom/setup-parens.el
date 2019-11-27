717ffa1ad5ed621914b5eb45843a1ee824072f04
Neil Chen <jingsi.chen@qq.com>
2019-11-27 08:25:27 +0800

    smartparens: only use smartparens-mode for programming
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

--- custom/setup-parens.el
+++ custom/setup-parens.el
@@ -2,14 +2,16 @@
 
 ;;; Code:
 
-;; highlight brackets
-(show-paren-mode 1)
-
 (use-package smartparens
+  :ensure t
+  :hook (prog-mode . smartparens-mode)
+  :custom
+  (sp-escape-quotes-after-insert nil)
   :config
-  (require 'smartparens-config)
-  (show-smartparens-global-mode +1)
-  (smartparens-global-mode 1))
+  (require 'smartparens-config))
+
+;; highlight brackets
+(show-paren-mode t)
 
 (provide 'setup-parens)
 ;;; $
