9bc4644043ac5f9a97684c8daadea829b800c7a0
Neil Chen <jingsi.chen@qq.com>
2019-11-27 08:35:41 +0800

    yasnippet: add classic snippets
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

--- custom/setup-yasnippet.el
+++ custom/setup-yasnippet.el
@@ -3,10 +3,14 @@
 ;;; Code:
 
 (use-package yasnippet
+  :ensure t
   :init
   (yas-global-mode 1))
 
-(use-package yasnippet-snippets)
+(use-package yasnippet-snippets
+  :ensure t)
+(use-package yasnippet-classic-snippets
+  :ensure t)
 
 (provide 'setup-yasnippet)
 ;;; $
